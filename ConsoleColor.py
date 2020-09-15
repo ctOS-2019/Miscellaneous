12b32f69474a67bdb4619f6de9f3728f1f1276ec
ctOS-2019 <726925273@qq.com>
2020-09-15 16:37:07 +0800

    version 0.0.2-base

--- ConsoleColor.py
+++ ConsoleColor.py
@@ -1,7 +1,10 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
 def ConsoleColor(text, fcolor=False, bcolor=False, h=False, u=False, s=False, f=False):
     """
     为pyCharm的控制台输出加上颜色
 
+    :param text: 待处理文本
     :param fcolor: 前景色
     :param bcolor: 背景色
     :param h: 高亮/加粗（默认否）
@@ -10,10 +13,12 @@ def ConsoleColor(text, fcolor=False, bcolor=False, h=False, u=False, s=False, f=
     :param f: 反显（默认否）
     :return: 返回样式设置
     """
-    fcdict = {'white': '30', 'red': '31', 'green': '32', 'yellow': '33', 'blue': '34', 'purple': '35', 'turquoise': '36', 'gray': '73'}
-    bcdict = {'white': '40', 'red': '41', 'green': '42', 'yellow': '43', 'blue': '44', 'purple': '45', 'turquoise': '46', 'gray': '47'}
+    fcdict = {'white': '30', 'red': '31', 'green': '32', 'yellow': '33', 'blue': '34', 'purple': '35',
+              'turquoise': '36', 'gray': '73'}
+    bcdict = {'white': '40', 'red': '41', 'green': '42', 'yellow': '43', 'blue': '44', 'purple': '45',
+              'turquoise': '46', 'gray': '47'}
     style = ''
-    style_list=[]
+    style_list = []
     if fcolor or bcolor or h or u or s or f:
         if h:
             style_list.append('1')
@@ -27,10 +32,11 @@ def ConsoleColor(text, fcolor=False, bcolor=False, h=False, u=False, s=False, f=
             style_list.append(fcdict[fcolor])
         if bcolor:
             style_list.append(bcdict[bcolor])
-        style=';'.join(style_list)
+        style = ';'.join(style_list)
     start = '\033['
     end = '\033[0m'
     return start + style + 'm' + text + end
 
+
 if __name__ == '__main__':
-    print(ConsoleColor('HelloWorld',fcolor='red', u=True,s=True))
+    print(ConsoleColor('HelloWorld', fcolor='red', u=True, s=True))
